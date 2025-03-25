957ecee44ab105027dc4f2a55aabdaa026801032
Nikhil Raj <nikhilraj13733@gmail.com>
2025-03-25 10:00:39 +0530

    initial provider creation

---
+++ lib/providers/dynamic_links_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class DynamicLinksProvider extends ChangeNotifier {}
---
+++ lib/providers/firestore_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class FirestoreProvider extends ChangeNotifier {}
